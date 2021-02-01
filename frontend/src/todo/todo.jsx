171f006668aff2736f9672289e7a4d3a2fe25e7b
Edilson Rodrigues <edarodrigs@gmail.com>
2021-02-01 16:01:33 -0300

    componentes todo e about

---
+++ frontend/src/todo/todo.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+export default class Todo extends Component {
+	render() {
+		return (
+			<div>
+				<h1>TODO</h1>
+			</div>
+		);
+	}
+}
