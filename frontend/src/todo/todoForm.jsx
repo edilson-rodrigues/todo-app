b7a2ea406977dee94fc303e08bbf38198a17073e
Edilson Rodrigues <edarodrigs@gmail.com>
2021-02-02 09:14:42 -0300

    estrutura do form

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -2,8 +2,19 @@ import React from "react";
 
 const TodoForm = (props) => {
 	return (
-		<div>
-			<h1>FORM</h1>
+		<div role="form" className="todoForm">
+			<div className="col-xs-12 col-sm-9 col-md-10">
+				<input
+					id="description"
+					className="form-control"
+					placeholder="Adcione uma tarefa"
+				/>
+			</div>
+			<div className="col-xs-12 col-sm-3 col-md-2">
+				<button className="btn btn-primary">
+					<i className="fa fa-plus"></i>
+				</button>
+			</div>
 		</div>
 	);
 };
