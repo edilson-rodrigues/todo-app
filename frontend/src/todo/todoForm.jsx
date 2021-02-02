efec0eef5b839056ccb8f9dc6c2dabe49cd74f88
Edilson Rodrigues <edarodrigs@gmail.com>
2021-02-02 15:30:17 -0300

    add teclas de atalhos

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -10,6 +10,16 @@ const TodoForm = (props) => {
 		handleClear,
 		description,
 	} = props;
+
+	const keyHandler = (e) => {
+		if (e.key === "Enter") {
+			//shit+enter ou enter
+			e.shiftKey ? handleSearch() : handleAdd();
+			//esc
+		} else if (e.key === "Escape") {
+			handleClear();
+		}
+	};
 	return (
 		<div role="form" style={{ paddingBottom: "60px" }}>
 			<Grid cols="12 9 10">
@@ -19,6 +29,7 @@ const TodoForm = (props) => {
 					placeholder="Adcione uma tarefa"
 					onChange={(e) => handleChange(e)}
 					value={description}
+					onKeyUp={keyHandler}
 				/>
 			</Grid>
 			<Grid cols="12 3 2">
